09708db4e239a8e2857e56d5cb5c12ea1ff8a6a3
AnilKumar3494 <karapaanilkumar005@gmail.com>
2024-04-04 11:18:10 -0400

    set-up and assg-1

---
+++ assg-1/assg-1/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textbox}>Hello, I'm Anil Kumar Karapa (ak4448). Using React Native to build Apps!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+
+  textbox: {
+    backgroundColor: '#ffeedd',
+    fontSize: 20,
+    fontWeight: "bold",
+
+  }
+});
