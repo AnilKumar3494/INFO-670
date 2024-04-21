540520c8c5594ac919da0b4b9bd7f18ebf573df1
AnilKumar3494 <karapaanilkumar005@gmail.com>
2024-04-21 09:57:56 -0400

    first assg and app

--- assg-1/assg-1/App.js
+++ assg-1/assg-1/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const userID = 'ak4448'
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textbox}>Hello, I'm Anil Kumar Karapa (ak4448). Using React Native to build Apps!</Text>
+      <Text style={styles.textbox}>`Hello, I'm Anil Kumar Karapa - ${userID} . Using React Native to build Apps!`</Text>
       <StatusBar style="auto" />
+      <Button title='Press me'>
+        Press me
+      </Button>
     </View>
+
   );
 }
 
@@ -23,6 +29,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffeedd',
     fontSize: 20,
     fontWeight: "bold",
-
   }
 });
--- first-react-native/App.js
+++ first-react-native/App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { useState } from 'react';
+import { Text, View, Image, Button, FlatList } from 'react-native';
+
+const Tiger = (prop) => {
+  const [isHungry, setIsHungry] = useState(true)
+  const tigerImageList = {
+    tiger1: {
+
+      link: "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3JvYl9yYXdwaXhlbF9jbG9zZS11cF9vbl9mYWNlX3RpZ2VyX2xvb2tpbmdfYXRfdGhlX2NhbWVyYV9mcm9udF8zODdhYzlhZC00ZWY5LTRiYzktODcyZC1lZjFiMjc0ODUzY2YuanBn.jpg"
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Yoooo React Native App on a Pixel!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={{
+        fontSize: 25
+      }}>Hello, I am {prop.name} </Text>
+      <Image source={{
+        uri: tigerImageList.tiger1.link,
+      }}
+        style={{
+          width: 100,
+          height: 100
+        }}
+      ></Image>
+      <Button
+        onPress={
+          () => {
+            setIsHungry(false)
+          }
+        }
+        title={isHungry ? "I am Hungry - Click to feed" : "Thanks for feeding, I am full :)"}
+
+        style={{
+
+        }}
+      ></Button>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tigers = () => {
+  return (
+    <View>
+      <FlatList
+        data={
+          [
+            { name: "Tiger 1" },
+            { name: "Tiger 2" },
+            { name: "Tiger 3" },
+          ]
+        }
+      >
+
+
+      </FlatList>
+      <Text>"Checking"</Text>
+
+
+    </View>
+  )
+}
+
+
+
+export default Tigers
